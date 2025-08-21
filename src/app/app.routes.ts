68469ec37931aeb2359bcafd3582f9f509d9736e
Cavin Smith <cavin@cavins-mbp.lan>
2025-08-21 10:56:45 +0200

    feat: fonts, colors, toolbar, constants for links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 import { ApiTest } from './components/api-test/api-test';
 import { ListLocations } from './components/list-locations/list-locations';
 import { ListEpisodes } from './components/list-episodes/list-episodes';
+
+import * as routesConstants from './constants/routes';
+
 export const routes: Routes = [
-  { path: '', component: ApiTest },
-  { path: 'locations', component: ListLocations },
-  { path: 'episodes', component: ListEpisodes }
+  { path: routesConstants.ROUTE_API_TEST, component: ApiTest },
+  { path: routesConstants.ROUTE_LOCATIONS, component: ListLocations },
+  { path: routesConstants.ROUTE_EPISODES, component: ListEpisodes }
   ];
--- src/app/components/toolbar/toolbar.ts
+++ src/app/components/toolbar/toolbar.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import * as routeConstants from '../../constants/routes';
+
+interface RouteLink {
+  path: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-toolbar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule],
   templateUrl: './toolbar.html',
-  styleUrl: './toolbar.scss'
+  styleUrl: './toolbar.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-
 export class Toolbar {
+  //routes = routeConstants;
+  links: RouteLink[] = [
+    { path: routeConstants.ROUTE_SEARCH, label: 'Search' },
+    { path: routeConstants.ROUTE_CHARACTERS, label: 'Dimensions' },
+    { path: routeConstants.ROUTE_LOCATIONS, label: 'Locations' },
+    { path: routeConstants.ROUTE_EPISODES, label: 'Episodes' },
+    { path: routeConstants.ROUTE_CHARACTERS, label: 'Characters' }
+  ];
 
 }
